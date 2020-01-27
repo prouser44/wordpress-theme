facea3d35f130ff82a1febdd3ead297f4d160528
prouser44 <ep4455@gmail.com>
2020-01-27 18:56:15 -0500

    404v4

--- 404.php
+++ 404.php
@@ -33,17 +33,12 @@ h1,p {
 	text-align: center;
 	padding-left: 30%;
 }
-.search-form{
-	text-align: center;
-}
+
 .image {
     position: relative;
     top: 10%;
     left: 10%;
     margin:-60px 0 0 -60px;
-    -webkit-animation:spin 4s linear infinite;
-    -moz-animation:spin 4s linear infinite;
-    animation:spin 4s linear infinite;
 }
 </style>
 </head>
@@ -58,39 +53,6 @@ h1,p {
 				
 				<div class="page-content">
 				<img class="image" src= "/epiper67/wordpress/wp-content/themes/wordpress-theme/assets/">
-				<br>
-				<br>
-				<br>
-					<p><?php esc_html_e( 'Nothing was found at this location. Try searching below.', 'hashtag' ); ?></p>
-						
-					<?php
-					get_search_form();
-
-					// the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<!-- <div class="widget widget_categories"> -->
-						<!-- <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'hashtag' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul> -->
-					<!-- </div>.widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					// $hashtag_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'hashtag' ), convert_smilies( ':)' ) ) . '</p>';
-					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$hashtag_archive_content" );
-
-					// the_widget( 'WP_Widget_Tag_Cloud' );
-					// ?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
