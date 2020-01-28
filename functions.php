0c81a174674e9ad5a8255982735b08f54a6dc090
prouser44 <ep4455@gmail.com>
2020-01-28 11:43:32 -0500

    Added theme support

--- functions.php
+++ functions.php
@@ -79,6 +79,8 @@ if ( ! function_exists( 'cheezeit_setup' ) ) :
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
+
+		add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
 	}
 endif;
 add_action( 'after_setup_theme', 'cheezeit_setup' );
